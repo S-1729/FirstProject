35b25451e4032ad64ede02db05bf8e9fc6258e23
S-1729 <srinivasmanikanta.pampana@gmail.com>
2026-09-06 14:42:02 +0530

    improved task1 -TL

--- SpringGitProject/src/test/java/com/nit/SpringGitProject/AppTest.java
+++ SpringGitProject/src/test/java/com/nit/SpringGitProject/AppTest.java
@@ -4,34 +4,34 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public void test1() {
 
-    
- 
-    public void test1() {
-    	
-    	App app=new App();
-    	assertEquals(30, app.sum(10, 20));
-    	
-    }
+		App app = new App();
+		assertEquals(30, app.sum(10, 20));
+
+	}
+
+	public void test2() {
+
+		App app = new App();
+		assertEquals(0, app.sum(0, 0));
+
+	}
 }
